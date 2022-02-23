e1b567fbe679ee5c69ecc5bc0ee3fa96434dad55
Hello Gu <gu.yp@qq.com>
2022-02-23 10:59:05 +0800

    解决objectPool重定义

--- ConcurrentMemoryPool/objectPool.h
+++ ConcurrentMemoryPool/objectPool.h
@@ -75,117 +75,117 @@ private:
 
 
 
-//测试定长内存池性能
-struct TreeNode
-{
-	int _val;
-	TreeNode* _left;
-	TreeNode* _right;
-
-	TreeNode()
-		:_val(0)
-		, _left(nullptr)
-		, _right(nullptr)
-	{}
-};
-
-void TestObjectPool()
-{
-
-	//验证还回来的内存是否重复利用的问题
-	ObjectPool<TreeNode> tnPool;
-	TreeNode* node1 = tnPool.New();
-	TreeNode* node2 = tnPool.New();
-	cout << node1 << endl;
-	cout << node2 << endl;
-
-	tnPool.Delete(node1);
-	TreeNode* node3 = tnPool.New();
-	cout << node3 << endl;
-
-	cout << endl;
-
-	//验证内存池到底快不快，有没有做到性能的优化
-
-	const size_t Rounds = 10;//申请释放的轮次
-	const size_t N = 100000;//每轮申请释放的次数
-
-	std::vector<TreeNode*> v1;
-	v1.reserve(N);
-
-#ifdef _WIN32
-	size_t begin1 = clock();
-#else
-	struct timeval begin1;
-	gettimeofday(&begin1, nullptr);
-#endif
-
-	for (size_t i = 0; i < Rounds; ++i) {
-		for (size_t i = 0; i < N; ++i)
-		{
-			v1.push_back(new TreeNode);
-		}
-		for (size_t i = 0; i < N; ++i)
-		{
-			delete v1[i];
-		}
-		v1.clear();
-	}
-
-#ifdef _WIN32
-	size_t end1 = clock();
-#else
-	struct timeval end1;
-	gettimeofday(&end1, nullptr);
-#endif
-
-
-	//这里我们调用自己所写的内存池
-	std::vector<TreeNode*> v2;
-	v2.reserve(N);
-	ObjectPool<TreeNode> tnPool1;
-
-#ifdef _WIN32
-	size_t begin2 = clock();
-#else
-	struct timeval begin2;
-	gettimeofday(&begin2, nullptr);
-#endif
-	for (size_t i = 0; i < Rounds; ++i) {
-		for (size_t i = 0; i < N; ++i)
-		{
-			v2.push_back(tnPool1.New());
-		}
-		for (size_t i = 0; i < N; ++i)
-		{
-			tnPool1.Delete(v2[i]);
-		}
-
-		v2.clear();
-	}
-
-#ifdef _WIN32
-	size_t end2 = clock();
-
-	cout << "new cost time:" << end1 - begin1 << endl;
-	cout << "object pool cost time:" << end2 - begin2 << endl;
-#else
-	//timeval结构体定义
-	/*
-	struct  timeval {
-		long  tv_sec;  //秒
-		long  tv_usec; //微妙
-	};
-	*/
-
-	struct timeval end2;
-	gettimeofday(&end2, nullptr);
-
-	//先将秒转微秒 再+不足1秒的微秒 = 总运行微秒数，再/1000换算为毫秒
-	double costTime1 = ((end1.tv_sec - begin1.tv_sec) * 1000000 + (end1.tv_usec - begin1.tv_usec)) / 1000;
-	double costTime2 = ((end2.tv_sec - begin2.tv_sec) * 1000000 + (end2.tv_usec - begin2.tv_usec)) / 1000;
-	
-	cout << "new cost time:" << costTime1 << endl;
-	cout << "object pool cost time:" << costTime2 << endl;
-#endif	 
-}
+////测试定长内存池性能
+//struct TreeNode
+//{
+//	int _val;
+//	TreeNode* _left;
+//	TreeNode* _right;
+//
+//	TreeNode()
+//		:_val(0)
+//		, _left(nullptr)
+//		, _right(nullptr)
+//	{}
+//};
+//
+//void TestObjectPool()
+//{
+//
+//	//验证还回来的内存是否重复利用的问题
+//	ObjectPool<TreeNode> tnPool;
+//	TreeNode* node1 = tnPool.New();
+//	TreeNode* node2 = tnPool.New();
+//	cout << node1 << endl;
+//	cout << node2 << endl;
+//
+//	tnPool.Delete(node1);
+//	TreeNode* node3 = tnPool.New();
+//	cout << node3 << endl;
+//
+//	cout << endl;
+//
+//	//验证内存池到底快不快，有没有做到性能的优化
+//
+//	const size_t Rounds = 10;//申请释放的轮次
+//	const size_t N = 100000;//每轮申请释放的次数
+//
+//	std::vector<TreeNode*> v1;
+//	v1.reserve(N);
+//
+//#ifdef _WIN32
+//	size_t begin1 = clock();
+//#else
+//	struct timeval begin1;
+//	gettimeofday(&begin1, nullptr);
+//#endif
+//
+//	for (size_t i = 0; i < Rounds; ++i) {
+//		for (size_t i = 0; i < N; ++i)
+//		{
+//			v1.push_back(new TreeNode);
+//		}
+//		for (size_t i = 0; i < N; ++i)
+//		{
+//			delete v1[i];
+//		}
+//		v1.clear();
+//	}
+//
+//#ifdef _WIN32
+//	size_t end1 = clock();
+//#else
+//	struct timeval end1;
+//	gettimeofday(&end1, nullptr);
+//#endif
+//
+//
+//	//这里我们调用自己所写的内存池
+//	std::vector<TreeNode*> v2;
+//	v2.reserve(N);
+//	ObjectPool<TreeNode> tnPool1;
+//
+//#ifdef _WIN32
+//	size_t begin2 = clock();
+//#else
+//	struct timeval begin2;
+//	gettimeofday(&begin2, nullptr);
+//#endif
+//	for (size_t i = 0; i < Rounds; ++i) {
+//		for (size_t i = 0; i < N; ++i)
+//		{
+//			v2.push_back(tnPool1.New());
+//		}
+//		for (size_t i = 0; i < N; ++i)
+//		{
+//			tnPool1.Delete(v2[i]);
+//		}
+//
+//		v2.clear();
+//	}
+//
+//#ifdef _WIN32
+//	size_t end2 = clock();
+//
+//	cout << "new cost time:" << end1 - begin1 << endl;
+//	cout << "object pool cost time:" << end2 - begin2 << endl;
+//#else
+//	//timeval结构体定义
+//	/*
+//	struct  timeval {
+//		long  tv_sec;  //秒
+//		long  tv_usec; //微妙
+//	};
+//	*/
+//
+//	struct timeval end2;
+//	gettimeofday(&end2, nullptr);
+//
+//	//先将秒转微秒 再+不足1秒的微秒 = 总运行微秒数，再/1000换算为毫秒
+//	double costTime1 = ((end1.tv_sec - begin1.tv_sec) * 1000000 + (end1.tv_usec - begin1.tv_usec)) / 1000;
+//	double costTime2 = ((end2.tv_sec - begin2.tv_sec) * 1000000 + (end2.tv_usec - begin2.tv_usec)) / 1000;
+//	
+//	cout << "new cost time:" << costTime1 << endl;
+//	cout << "object pool cost time:" << costTime2 << endl;
+//#endif	 
+//}
